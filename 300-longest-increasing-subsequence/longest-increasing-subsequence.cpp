912e8f06eccd2172c18e7df1b771546c945bfb93
Shivam Chaudhary <shivamthalwal@gmail.com>
2023-07-15 20:44:33 +0100

    Time: 758 ms (32.00%) | Memory: 292.1 MB (26.30%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,26 +1,20 @@
 class Solution {
-private: 
-    int solveIncreaing(int index, int prevIndex, int &n, vector<int> &nums, vector<vector<int>> &dp) {
-        if(index == n) return 0;
-
-        if(dp[index][prevIndex+1]!=-1)
-          return dp[index][prevIndex+1];
-
-        int take=INT_MIN, notTake;
-
-        // not take
-        notTake = solveIncreaing(index+1, prevIndex, n, nums, dp);
-
-        // take
-        if(prevIndex == -1 || nums[prevIndex] < nums[index])
-          take = 1 + solveIncreaing(index+1, index, n, nums, dp);
-
-        return dp[index][prevIndex+1] = max(notTake, take);
-    }
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return solveIncreaing(0, -1, n, nums, dp);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        
+        for(int index = n-1; index>=0; index--) {
+           for(int prevIndex= index-1; prevIndex>=-1; prevIndex--) {
+               int take =INT_MIN;
+               
+               if(prevIndex == -1 || nums[prevIndex] < nums[index])
+                  take = 1 + dp[index+1][index+1];
+
+               dp[index][prevIndex+1] = max(dp[index+1][prevIndex+1], take);
+           }
+        }
+
+        return dp[0][0];
     }
 };
