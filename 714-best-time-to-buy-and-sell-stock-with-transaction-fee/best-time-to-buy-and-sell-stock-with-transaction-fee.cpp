48e1e06ddf1d175a7b1ece79de0736da144f469c
Shivam Chaudhary <shivamthalwal@gmail.com>
2023-07-15 15:16:10 +0100

    Time: 246 ms (37.36%) | Memory: 77.1 MB (43.14%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,30 +1,30 @@
 class Solution {
-private:
-    int profit(int index,int check,int fee, vector<int> &prices, vector<vector<int>> &dp) {
-        if(index == prices.size()) return 0;
-
-        if(dp[index][check]!=-1) return dp[index][check];
-
-        long profitSum = 0;
-        if(check == 0)
-        profitSum = max(
-                        0 + profit(index+1, 0,fee, prices, dp),
-                        - prices[index] + profit(index+1, 1,fee, prices, dp)
-                    );
-        if(check == 1)
-        profitSum = max(
-                       0 + profit(index+1, 1,fee, prices, dp),
-                       prices[index] - fee + profit(index+1, 0,fee, prices, dp)
-                    );
-
-        return dp[index][check] = (int)profitSum;
-    }
-
 public:
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        if(n==0) return 0;
-       return  profit(0,0,fee, prices, dp);
+        vector<int> dp(2,-1);
+        dp[0] = dp[1] =0;
+
+        for(int index = n-1; index>=0; index--) {
+            vector<int> cur(2,-1);
+            for(int check= 0; check<=1; check++) {
+                 long profitSum = 0;
+                  if(check == 0)
+                  profitSum = max(
+                                  0 + dp[0],
+                                  - prices[index] + dp[1]
+                              );
+                  if(check == 1)
+                  profitSum = max(
+                                 0 + dp[1],
+                                 prices[index] - fee + dp[0]
+                              );
+
+                  cur[check] = (int)profitSum;
+            }
+            dp = cur;
+        }
+
+        return dp[0];
     }
 };
