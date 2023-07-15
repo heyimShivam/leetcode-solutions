851b61af118cc67fe316dc8d9f3221389e0647b6
Shivam Chaudhary <shivamthalwal@gmail.com>
2023-07-15 12:53:22 +0100

    Time: 12 ms (38.15%) | Memory: 15.1 MB (29.86%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,29 +1,28 @@
 class Solution {
-private:
-    int profit(int index,int check, vector<int> &prices, vector<vector<int>> &dp) {
-        if(index == prices.size()) return 0;
-
-        if(dp[index][check]!=-1) return dp[index][check];
-
-        long profitSum = 0;
-        if(check == 0)
-        profitSum = max(
-                        0 + profit(index+1, 0, prices, dp),
-                        - prices[index] + profit(index+1, 1, prices, dp)
-                    );
-        if(check == 1)
-        profitSum = max(
-                       0 + profit(index+1, 1, prices, dp),
-                       prices[index] + profit(index+1, 0, prices, dp)
-                    );
-
-        return dp[index][check] = (int)profitSum;
-    }
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        if(n==0) return 0;
-       return  profit(0,0, prices, dp);
+       int n = prices.size();
+        vector<vector<int>> dp(n + 1,vector<int>(2,-1));
+        dp[n][0] = dp[n][1] =0;
+
+        for(int index = n-1; index>=0; index--) {
+            for(int check= 0; check<=1; check++) {
+                 long profitSum = 0;
+                  if(check == 0)
+                  profitSum = max(
+                                  0 + dp[index+1][0],
+                                  - prices[index] + dp[index+1][1]
+                              );
+                  if(check == 1)
+                  profitSum = max(
+                                 0 + dp[index+1][1],
+                                 prices[index] + dp[index+1][0]
+                              );
+
+                  dp[index][check] = (int)profitSum;
+            }
+        }
+
+        return dp[0][0];
     }
 };
