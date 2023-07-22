fd0cee4aed1aa0facfae3a8fa905b43882f2834c
Shivam Chaudhary <shivamthalwal@gmail.com>
2023-07-22 16:31:41 +0100

    Time: 23 ms (50.58%) | Memory: 8.7 MB (67.87%) - LeetSync

---
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    double solve(int indexRow, int indexCol, int n, int moves, vector<vector<vector<double>>> &dp) {
+        if(indexRow<0 || indexRow>=n || indexCol<0 || indexCol>=n) return 0;
+        if(dp[indexRow][indexCol][moves] != -1) return dp[indexRow][indexCol][moves];
+        if(moves == 0) return 1;
+        
+        double chanceSum = 0;
+
+        chanceSum += solve(indexRow - 2, indexCol-1, n, moves -1 ,dp );
+        chanceSum += solve(indexRow - 2, indexCol+1, n, moves -1 ,dp );
+        chanceSum +=solve(indexRow + 2, indexCol-1, n, moves -1 ,dp );
+        chanceSum += solve(indexRow + 2, indexCol+1, n, moves -1 ,dp );
+        chanceSum += solve(indexRow - 1, indexCol-2, n, moves -1 ,dp );
+        chanceSum += solve(indexRow +1, indexCol-2, n, moves -1 ,dp ); 
+        chanceSum += solve(indexRow -1, indexCol+2, n, moves -1 ,dp ); 
+        chanceSum += solve(indexRow + 1, indexCol+2, n, moves -1 ,dp );
+
+        return dp[indexRow][indexCol][moves] = chanceSum/8;
+    }
+
+    double knightProbability(int n, int k, int row, int column) {
+        vector<vector<vector<double>>> dp(n+1, vector<vector<double>>(n+1, vector<double>(k+1, -1)));
+        return solve(row, column, n, k, dp);
+    }
+};
