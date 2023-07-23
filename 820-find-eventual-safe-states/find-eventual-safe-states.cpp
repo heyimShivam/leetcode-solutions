f3c8d8a1a313485ec35cf60d434adc9e3f6146a9
Shivam Chaudhary <77006897+heyimShivam@users.noreply.github.com>
2023-07-23 19:39:23 +0100

    Time: 232 ms (34.24%) | Memory: 60.7 MB (35.94%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,49 +1,55 @@
 class Solution {
+private:
+	bool dfsCheck(int node, vector<int> adj[], vector<int> &vis,
+ vector<int> &pathVis, 
+        vector<int> &check) {
+		vis[node] = 1;
+		pathVis[node] = 1;
+		check[node] = 0;
+		for (auto it : adj[node]) {
+			if (!vis[it]) {
+			if (dfsCheck(it, adj, vis, pathVis, check) == true) {
+					check[node] = 0;
+					return true;
+				}
+
+			} else if (pathVis[it]) {
+				check[node] = 0;
+				return true;
+			}
+		}
+		check[node] = 1;
+		pathVis[node] = 0;
+		return false;
+	}
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
+        int V = graph.size();
 
-        vector<int> adjoint[n];
+        vector<int> adj[V];
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < V; i++) {
             for(auto it: graph[i]) {
-                adjoint[it].push_back(i);
+                adj[i].push_back(it);
             }
         }
 
-        vector<int> indgree(n, 0);
+        vector<int> vis(V, 0);
+		    vector<int> pathVis(V, 0);
+		    vector<int> check(V, 0);
+		
+        vector<int> safeNodes;
+  		
+        for (int i = 0; i < V; i++) {
+			    if (!vis[i]) {
+			    	dfsCheck(i, adj, vis, pathVis, check);
+			    }
+		    }
+		
+        for (int i = 0; i < V; i++) {
+			    if (check[i] == 1) safeNodes.push_back(i);
+		    }
 
-        for(int i=0; i<n; i++) {
-            for(auto it: adjoint[i]) {
-                indgree[it]++;
-            }
-        }
-
-        queue<int> q;
-
-        for(int i=0 ;i<n; i++) {
-            if(indgree[i] == 0) {
-                q.push(i);
-            }
-        }
-
-        vector<int> topo;
-
-        while( !q.empty() ) {
-            int node = q.front();
-            q.pop();
-            topo.push_back(node);
-
-            for(auto it: adjoint[node]) {
-                indgree[it]--;
-                if(indgree[it] == 0) {
-                    q.push(it);
-                }
-            }
-        }
-
-        sort(topo.begin(), topo.end());
-        
-        return topo;
+		return safeNodes;
     }
 };
