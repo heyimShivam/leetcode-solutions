d6f80f3e5e2e4e9781ee2be18295574408a2dabe
Shivam Chaudhary <77006897+heyimShivam@users.noreply.github.com>
2023-07-24 20:28:08 +0100

    Time: 0 ms (100.00%) | Memory: 5.9 MB (62.65%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0) return 1;
+
+        if(n<0) {
+            x = 1 /x;
+            n = abs(n);
+        }
+
+        if(n%2==0) {
+            return myPow(x * x, n/2);
+        }
+        return x * myPow(x * x, n/2);
+    }
+};
