0cbf929d916143ff62c03ac71bdf12d83f220ff5
Shivam Chaudhary <77006897+heyimShivam@users.noreply.github.com>
2023-08-13 00:40:00 +0530

    Time: 0 ms (100.00%) | Memory: 6.9 MB (89.67%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -0,0 +1,32 @@
+class MyQueue {
+    stack<int> s1, s2;
+public:
+    MyQueue() {
+    }
+    
+    void push(int x) {
+        while (!s1.empty()) {
+            s2.push(s1.top());
+            s1.pop();
+        }
+        s1.push(x);
+        while (!s2.empty()) {
+            s1.push(s2.top());
+            s2.pop();
+        }
+    }
+    
+    int pop() {
+        int ans = s1.top();
+        s1.pop();
+        return ans;
+    }
+    
+    int peek() {
+        return s1.top();
+    }
+    
+    bool empty() {
+        return s1.empty();
+    }
+};
