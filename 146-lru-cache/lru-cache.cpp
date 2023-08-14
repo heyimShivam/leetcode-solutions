04a80c9cff6f98ee85310ce01cedfb6cad445500
Shivam Chaudhary <77006897+heyimShivam@users.noreply.github.com>
2023-08-14 21:33:44 +0530

    Time: 425 ms (91.02%) | Memory: 181.4 MB (7.58%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,62 +1,78 @@
-struct Node{
+struct Node {
     int key;
-    int val;
-    Node* next = NULL;
-    Node* prev = NULL;
-    Node(int _key, int value){
+    int value;
+    
+    Node *prev =NULL;
+    Node *next =NULL;
+
+    Node(int _key, int _value) {
         key = _key;
-        val = value;
+        value = _value;
     }
 };
 
 class LRUCache {
 public:
+    int cap = 0;
+    unordered_map<int, Node*> mp;
+
     Node* head = new Node(-1,-1);
     Node* tail = new Node(-1,-1);
 
-    unordered_map<int,Node*>mp;
-    int cap;
     LRUCache(int capacity) {
+        cap = capacity;
         head->next = tail;
         tail->prev = head;
-        cap = capacity;
-    }
-
-    void deleteNode(int key){
-        Node* node = mp[key];
-        Node* prevNode = node->prev;
-        Node* nextNode = node->next;
-        prevNode->next = nextNode;
-        nextNode->prev = prevNode;
-        mp.erase(key);
-    }
-
-    void insert(int key, int value){
-        Node* temp = head->next;
-        Node* newNode = new Node(key, value);
-        head->next = newNode;
-        newNode->next = temp;
-        temp->prev = newNode;
-        newNode->prev = head;
-        mp[key] = newNode;
     }
     
+    void deleteNode(int key) {
+        Node *del = mp[key];
+        Node* prevNode = del->prev;
+        Node* nextNode = del->next;
+
+        prevNode->next = nextNode;
+        nextNode->prev = prevNode;
+
+        mp.erase(key);
+        // free(del);
+    }
+
+    void insert(int key, int value) {
+       Node *newNode = new Node(key, value);
+       
+       newNode->next = head->next;
+       head->next = newNode;
+       newNode->prev = head;
+       newNode->next->prev = newNode;
+       mp[key] = newNode;
+        // Node* temp = head->next;
+        // Node* newNode = new Node(key, value);
+        // head->next = newNode;
+        // newNode->next = temp;
+        // temp->prev = newNode;
+        // newNode->prev = head;
+        // mp[key] = newNode;
+    }
+
     int get(int key) {
-        if(mp.find(key) != mp.end()){
-            int value = mp[key]->val;
+        if(mp.find(key) != mp.end()) {
+            int value = mp[key]->value;
+
             deleteNode(key);
             insert(key,value);
+            
             return value;
         }
+
         return -1;
     }
     
     void put(int key, int value) {
-        if(mp.find(key) != mp.end()){
+        if(mp.find(key) != mp.end()) {
             deleteNode(key);
         }
 
-        if(mp.size() >= cap){
+        if(mp.size() >= cap) {
             deleteNode(tail->prev->key);
         }
 
