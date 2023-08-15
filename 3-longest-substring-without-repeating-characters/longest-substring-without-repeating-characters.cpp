1e77f682db1f341a909574bfc56467d07b883c64
Shivam Chaudhary <77006897+heyimShivam@users.noreply.github.com>
2023-08-15 17:49:23 +0530

    Time: 29 ms (37.41%) | Memory: 13.6 MB (11.48%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,45 +1,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s == "") return 0;
+      if(s == "") return 0;
 
-        set<char> st;
-        int i=0;
-        int j=0;
-        int maxLen = 0;
-        while(j < s.size()) {
-            if(st.find(s[j]) == st.end()) {
-              st.insert(s[j]);
+      set<char> st;
 
-              maxLen = maxLen > st.size() ? maxLen : st.size();
-              
-              j++;
-            } else {
-              st.erase(st.find(s[i]));
-              i++;
-            }
-        }
+      int i=0;
+      int j=0;
+      int maxLen = 0;
 
-        return maxLen;
+      while(j < s.size()) {
+          if(st.find(s[j]) == st.end()) {
+            st.insert(s[j]);
+            maxLen = maxLen > st.size() ? maxLen : st.size();
+            j++;
+          } else {
+            st.erase(st.find(s[i]));
+            i++;
+          }
+      }
+
+      return maxLen;
     }
 };
-
-// class Solution {
-// public:
-//     int lengthOfLongestSubstring(string s) {
-//       unordered_map<char, int> mp;
-//       int len = 0;
-//       if(s == "") {return 0;}
-//       for(int i=0; i< s.length(); i++) {
-//         if(mp.find(s[i]) != mp.end()) {
-//           mp[s[i]] = i;
-//           int temp = s.length()-1-i;
-//           len = max(len, temp);
-//           // mp.insert({s[i], i});
-//         } else {
-//           mp.insert({s[i], i});
-//         }
-//       }
-//         return len;
-//     }
-// };
